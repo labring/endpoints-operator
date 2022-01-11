2201b470e4e13b2f14d308db7c3d1270b2c8d281
cuisongliu <cuisongliu@qq.com>
2022-01-11 11:13:29 +0800

    feat(main): init commit

--- library/version/version.go
+++ library/version/version.go
@@ -23,10 +23,10 @@ var (
 	// NOTE: The $Format strings are replaced during 'git archive' thanks to the
 	// companion .gitattributes file containing 'export-subst' in this same
 	// directory.  See also https://git-scm.com/docs/gitattributes
-	GitVersion = "v0.0.0-master+unknown"
-	GitCommit  = "unknown" // sha1 from git, output of $(git rev-parse HEAD)
+	gitVersion = "v0.0.0-master+unknown"
+	gitCommit  = "unknown" // sha1 from git, output of $(git rev-parse HEAD)
 
-	BuildDate = "1970-01-01T00:00:00Z" // build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
+	buildDate = "1970-01-01T00:00:00Z" // build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
 )
 
 // Info holds build information
@@ -42,9 +42,9 @@ type Info struct {
 // Get creates and initialized Info object
 func Get() Info {
 	return Info{
-		GitCommit:  GitCommit,
-		GitVersion: GitVersion,
-		BuildDate:  BuildDate,
+		GitCommit:  gitCommit,
+		GitVersion: gitVersion,
+		BuildDate:  buildDate,
 		GoVersion:  runtime.Version(),
 		Compiler:   runtime.Compiler,
 		Platform:   fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
